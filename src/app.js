3be14c7354f1a6a19dc12799eed0ab241bb4bfeb
Johanna Eriksson <je224gs@student.lnu.se>
2024-09-17 12:54:13 +0200

    Initial commit

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
-console.log("Hello, world!");
+for(let i = 1; i <= 100; i++) {
+  if(i % 3 === 0 && i % 5 === 0) {
+    console.log('fizzbuzz')
+    continue
+  }
+  if(i % 3 === 0) {
+    console.log('fizz')
+    continue
+  }
+  if(i % 5 === 0) {
+    console.log('buzz')
+    continue
+  }
+  console.log(i)
+}

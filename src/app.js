7408afeae4f90fc297b43648b19becfcdd6c4baf
Johanna Eriksson <je224gs@student.lnu.se>
2024-09-17 13:03:16 +0200

    Second try

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 for(let i = 1; i <= 100; i++) {
-  if(i % 3 === 0 && i % 5 === 0) {
-    console.log('fizzbuzz')
-    continue
-  }
+  let output = ''
   if(i % 3 === 0) {
-    console.log('fizz')
-    continue
+    output = 'fizz'
   }
   if(i % 5 === 0) {
-    console.log('buzz')
-    continue
+    output += 'buzz'
   }
-  console.log(i)
+  
+  if(output != '') {
+    console.log(output)
+  } else {
+    console.log(i)
+  }
 }
